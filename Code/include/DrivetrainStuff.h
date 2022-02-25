511d03a274fee919d0e51c0c55223706b4e2eaa4
Pi-way <caleb.carlson@student.besd.net>
2022-02-24 20:06:02 -0700

    2/24/22
    Cleaned up DrivetrainStuff.h

--- Code/include/DrivetrainStuff.h
+++ Code/include/DrivetrainStuff.h
@@ -25,12 +25,10 @@ extern bool Wait;
 extern float Speed_V;
 extern float Drive_balance;
 
-
-
-#define SetDriveBrake(brake_mode)       \
-  BRMotor.setStopping(brake_mode);      \
-  BLMotor.setStopping(brake_mode);      \
-  FRMotor.setStopping(brake_mode);      \
+#define SetDriveBrake(brake_mode)                     \
+  BRMotor.setStopping(brake_mode);                    \
+  BLMotor.setStopping(brake_mode);                    \
+  FRMotor.setStopping(brake_mode);                    \
   FLMotor.setStopping(brake_mode);
 
 #define SetDrivePosition(DrivePosition)               \
@@ -39,10 +37,10 @@ extern float Drive_balance;
   FRMotor.setPosition((DrivePosition), degrees);      \
   FLMotor.setPosition((DrivePosition), degrees);
 
-#define Drivetrain( MotorMember )            \
-  BRMotor.MotorMember                      \
-  BLMotor.MotorMember                      \
-  FRMotor.MotorMember                      \
+#define Drivetrain( MotorMember )                     \
+  BRMotor.MotorMember                                 \
+  BLMotor.MotorMember                                 \
+  FRMotor.MotorMember                                 \
   FLMotor.MotorMember
 
 #define Rads(Degs) ((3.1415926535/180.0)*Degs)
@@ -51,12 +49,13 @@ extern float Drive_balance;
 #define MinVoltage 1.5
 
 float GetClosestToZero(float First, float Second);
+
 void Calibrate();
+void StartTasks();
+
 void Turn(float Turn_, float speed = 100, bool Wait_ = true, float CoustomTimeout_ = 2.0);
 void TurnTo(float Turn_x, float Turn_y, float speed = 100, bool Wait_ = true, float Turn_Degree = 0, float Coustom_Timeout = 2.5);
 void Drive(float Distance_, float Speed_V_ = 100, bool Wait_ = true, bool f_b = false, bool b_b = false, bool coast = false, float maxDistance = 10000000);
 void DriveFast(float Distance_, float Speed_V_ = 100, bool Wait_ = true, bool f_b = false, bool b_b = false, bool coast = false, float maxDistance = 10000000);
-
 void DriveTo(float Drive_x, float Drive_y, float speed, float radius = 0, bool Wait_ = true, float Coustom_Timeout = 5);
-void StartTasks();
 void TurnAndDrive(float x_point, float y_point, float driveSpeed = 80, float turnSpeed = 80, float radius = 0, bool faceDirection = true, bool driveWait = true, float TurnTimeout = 2.5, float DriveTimeout = 5);
